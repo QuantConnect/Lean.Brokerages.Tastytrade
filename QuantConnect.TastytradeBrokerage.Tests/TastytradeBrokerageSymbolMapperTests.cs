0320576b2aa9516b1dd9cd10ee7bec3f8a04c54d
Romazes <romazes20@gmail.com>
2025-05-26 17:25:30 +0300

    feat:SymbolMapper:GetBrokerage: Equity, Index, Option, IndexOption, Future test:feat:SymbolMapper:GetBrokerage: Equity, Index, Option, IndexOption, Future

--- QuantConnect.TastytradeBrokerage.Tests/TastytradeBrokerageSymbolMapperTests.cs
+++ QuantConnect.TastytradeBrokerage.Tests/TastytradeBrokerageSymbolMapperTests.cs
@@ -13,22 +13,106 @@
  * limitations under the License.
 */
 
+using System;
 using NUnit.Framework;
+using QuantConnect.Tests;
+using QuantConnect.Securities;
+using System.Collections.Generic;
 
 namespace QuantConnect.Brokerages.Tastytrade.Tests;
 
-[TestFixture, Ignore("Not implemented")]
+[TestFixture]
 public class TastytradeBrokerageSymbolMapperTests
 {
+    /// <summary>
+    /// Provides the mapping between Lean symbols and brokerage specific symbols.
+    /// </summary>
+    private TastytradeBrokerageSymbolMapper _symbolMapper;
+
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
+    {
+        _symbolMapper = new(TestSetup.CreateTastytradeApiClient());
+    }
+
     [Test]
     public void ReturnsCorrectLeanSymbol()
     {
 
     }
 
-    [Test]
-    public void ReturnsCorrectBrokerageSymbol()
+    private static IEnumerable<TestCaseData> LeanSymbolTestCases
     {
+        get
+        {
+            TestGlobals.Initialize();
+            // TSLA - Equity
+            var underlying = Symbol.Create("TSLA", SecurityType.Equity, Market.USA);
+            yield return new TestCaseData(underlying, "TSLA", "TSLA");
+            yield return new TestCaseData(Symbol.CreateOption(underlying, Market.USA, OptionStyle.American, OptionRight.Call, 345.0m, new DateTime(2025, 06, 20)), "TSLA  250620C00345000", ".TSLA250620C345");
+            yield return new TestCaseData(Symbol.CreateOption(underlying, Market.USA, OptionStyle.American, OptionRight.Put, 5m, new DateTime(2026, 12, 18)), "TSLA  261218P00005000", ".TSLA261218P5");
+            // SPX - Index
+            var SPX = Symbol.Create("SPX", SecurityType.Index, Market.USA);
+            yield return new TestCaseData(SPX, "SPX", "SPX");
+            yield return new TestCaseData(Symbol.CreateOption(SPX, Market.USA, SecurityType.IndexOption.DefaultOptionStyle(), OptionRight.Call,
+                5050m, new DateTime(2025, 09, 19)), "SPX   250919C05050000", ".SPX250919C5050");
+            //// SPXW - Index
+            yield return new TestCaseData(Symbol.CreateOption(SPX, "SPXW", Market.USA, SecurityType.IndexOption.DefaultOptionStyle(), OptionRight.Call,
+                3500m, new DateTime(2025, 07, 18)), "SPXW  250718C03500000", ".SPXW250718C3500");
+            // F - Equity
+            var F = Symbol.Create("F", SecurityType.Equity, Market.USA);
+            yield return new TestCaseData(F, "F", "F");
+            yield return new TestCaseData(Symbol.CreateOption(F, Market.USA, SecurityType.Option.DefaultOptionStyle(), OptionRight.Call, 2.02m, new DateTime(2025, 06, 20)), "F     250620C00002020", ".F250620C2.02");
+            yield return new TestCaseData(Symbol.CreateOption(F, Market.USA, SecurityType.Option.DefaultOptionStyle(), OptionRight.Put, 2.17m, new DateTime(2025, 06, 20)), "F     250620P00002170", ".F250620P2.17");
+            // DJT - Equity
+            var DJT = Symbol.Create("DJT", SecurityType.Equity, Market.USA);
+            yield return new TestCaseData(DJT, "DJT", "DJT");
+            yield return new TestCaseData(Symbol.CreateOption(DJT, Market.USA, SecurityType.Option.DefaultOptionStyle(), OptionRight.Call, 13.0m, new DateTime(2025, 12, 19)), "DJT   251219C00013000", ".DJT251219C13");
+            // IRBT - Equity
+            var IRBT = Symbol.Create("IRBT", SecurityType.Equity, Market.USA);
+            yield return new TestCaseData(IRBT, "IRBT", "IRBT");
+            yield return new TestCaseData(Symbol.CreateOption(IRBT, Market.USA, SecurityType.Option.DefaultOptionStyle(), OptionRight.Call, 22.5m, new DateTime(2026, 1, 16)), "IRBT  260116C00022500", ".IRBT260116C22.5");
+            // GOOGL - Equity
+            var GOOGL = Symbol.Create("GOOGL", SecurityType.Equity, Market.USA);
+            yield return new TestCaseData(GOOGL, "GOOGL", "GOOGL");
+            yield return new TestCaseData(Symbol.CreateOption(GOOGL, Market.USA, SecurityType.Option.DefaultOptionStyle(), OptionRight.Put, 235m, new DateTime(2027, 06, 17)), "GOOGL 270617P00235000", ".GOOGL270617P235");
+            // BRK.B - Equity
+            var BRK_B = Symbol.Create("BRK.B", SecurityType.Equity, Market.USA);
+            yield return new TestCaseData(BRK_B, "BRK/B", "BRK/B");
+            yield return new TestCaseData(Symbol.CreateOption(BRK_B, BRK_B.ID.Market, SecurityType.Option.DefaultOptionStyle(), OptionRight.Call, 200m, new DateTime(2025, 06, 20)), "BRKB  250620C00200000", ".BRKB250620C200");
+            yield return new TestCaseData(Symbol.CreateOption(BRK_B, BRK_B.ID.Market, SecurityType.Option.DefaultOptionStyle(), OptionRight.Put, 690m, new DateTime(2026, 03, 20)), "BRKB  260320P00690000", ".BRKB260320P690");
+            // AAM.U - Equity
+            var AAM_U = Symbol.Create("AAM.U", SecurityType.Equity, Market.USA);
+            yield return new TestCaseData(AAM_U, "AAM/U", "AAM/U");
 
+            // ES - Future
+            var SP500EMini = Symbol.CreateFuture(Futures.Indices.SP500EMini, Market.CME, new DateTime(2025, 06, 20));
+            yield return new(SP500EMini, "/ESM5", "/ESM25:XCME");
+
+            // GC - Future
+            var Gold = Symbol.CreateFuture(Futures.Metals.Gold, Market.CME, new DateTime(2025, 12, 29));
+            yield return new(Gold, "/GCZ5", "/GCZ25:XCEC");
+
+            // GBP - Future
+            var GBP = Symbol.CreateFuture(Futures.Currencies.GBP, Market.CME, new DateTime(2025, 12, 15));
+            yield return new(GBP, "/6BZ5", "/6BZ25:XCME");
+
+            // Gasoline - Future
+            var Gasoline = Symbol.CreateFuture(Futures.Energy.Gasoline, Market.CME, new DateTime(2025, 06, 1));
+            yield return new(Gasoline, "/RBM5", "/RBM25:XNYM");
+        }
+    }
+
+    [Test, TestCaseSource(nameof(LeanSymbolTestCases))]
+    public void ReturnsCorrectBrokerageSymbol(Symbol symbol, string expectedBrokerageSymbol, string expectedBrokerageStreamSymbol)
+    {
+        var (brokerageSymbol, brokerageStreamMarketDataSymbol) = _symbolMapper.GetBrokerageSymbols(symbol);
+
+        Assert.IsNotNull(brokerageSymbol);
+        Assert.IsNotEmpty(brokerageSymbol);
+        Assert.AreEqual(expectedBrokerageSymbol, brokerageSymbol);
+        Assert.IsNotEmpty(brokerageStreamMarketDataSymbol);
+        Assert.IsNotNull(brokerageStreamMarketDataSymbol);
+        Assert.AreEqual(expectedBrokerageStreamSymbol, brokerageStreamMarketDataSymbol);
     }
 }
---
+++ QuantConnect.TastytradeBrokerage/TastytradeBrokerage.SymbolMapper.cs
@@ -0,0 +1,176 @@
+﻿/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using QuantConnect.Brokerages.Tastytrade.Api;
+using QuantConnect.Brokerages.Tastytrade.Models;
+
+namespace QuantConnect.Brokerages.Tastytrade;
+
+/// <summary>
+/// Maps Lean symbols to Tastytrade-compatible brokerage symbols used for orders and streaming market data.
+/// </summary>
+public class TastytradeBrokerageSymbolMapper
+{
+    /// <summary>
+    /// The Tastytrade API client used to query instrument and market data.
+    /// </summary>
+    private readonly TastytradeApiClient _tastytradeApiClient;
+
+    /// <summary>
+    /// A cache mapping Lean symbols to their corresponding brokerage and stream symbols.
+    /// </summary>
+    private readonly ConcurrentDictionary<Symbol, BrokerageSymbols> _brokerageSymbolsByLeanSymbol = [];
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TastytradeBrokerageSymbolMapper"/> class.
+    /// </summary>
+    /// <param name="tastytradeApiClient">The Tastytrade API client used to query future instruments.</param>
+    public TastytradeBrokerageSymbolMapper(TastytradeApiClient tastytradeApiClient)
+    {
+        _tastytradeApiClient = tastytradeApiClient;
+    }
+
+    /// <summary>
+    /// Returns the brokerage symbols used for orders and streaming data based on the Lean <see cref="Symbol"/>.
+    /// </summary>
+    /// <param name="symbol">The Lean symbol to map.</param>
+    /// <returns>
+    /// A tuple containing:
+    /// <list type="bullet">
+    /// <item><description>The brokerage symbol used for placing orders.</description></item>
+    /// <item><description>The brokerage symbol used for streaming market data.</description></item>
+    /// </list>
+    /// </returns>
+    /// <exception cref="NotSupportedException">Thrown if the <paramref name="symbol"/> security type is not supported.</exception>
+    public (string brokerageSymbol, string brokerageStreamMarketDataSymbol) GetBrokerageSymbols(Symbol symbol)
+    {
+        if (_brokerageSymbolsByLeanSymbol.TryGetValue(symbol, out var brokerageSymbols))
+        {
+            return (brokerageSymbols.BrokerageSymbol, brokerageSymbols.BrokerageStreamMarketDataSymbol);
+        }
+
+        var brokerageSymbol = default(string);
+        var brokerageStreamMarketDataSymbol = default(string);
+        switch (symbol.SecurityType)
+        {
+            case SecurityType.Equity:
+            case SecurityType.Index:
+                brokerageSymbol = ToBrokerageTickerFormat(symbol.Value);
+                brokerageStreamMarketDataSymbol = brokerageSymbol;
+                break;
+            case SecurityType.Option:
+            case SecurityType.IndexOption:
+                (brokerageSymbol, brokerageStreamMarketDataSymbol) = GenerateOptionBrokerageSymbols(symbol);
+                break;
+            case SecurityType.Future:
+                (brokerageSymbol, brokerageStreamMarketDataSymbol) = GenerateFutureBrokerageSymbols(symbol).SynchronouslyAwaitTaskResult();
+                break;
+            default:
+                throw new NotSupportedException($"{nameof(TastytradeBrokerageSymbolMapper)}.{nameof(GetBrokerageSymbols)}.");
+        }
+
+        _brokerageSymbolsByLeanSymbol[symbol] = new(brokerageSymbol, brokerageStreamMarketDataSymbol);
+
+        return (brokerageSymbol, brokerageStreamMarketDataSymbol);
+    }
+
+    /// <summary>
+    /// Generates the brokerage and streaming symbols for an option or index option.
+    /// </summary>
+    /// <param name="symbol">The Lean option symbol.</param>
+    /// <returns>A tuple with the brokerage symbol and the streaming market data symbol.</returns>
+    private static (string brokerageSymbol, string brokerageStreamMarketDataSymbol) GenerateOptionBrokerageSymbols(Symbol symbol)
+    {
+        var underlying = ToOptionBrokerageTickerFormat(symbol.Canonical.Value.Replace("?", string.Empty));
+        var expiryDate = symbol.ID.Date.ToStringInvariant(DateFormat.SixCharacter);
+        var optionRight = symbol.ID.OptionRight.ToString()[0];
+
+        if (underlying.Length > 5)
+        {
+            underlying += " ";
+        }
+
+        return ($"{underlying,-6}{expiryDate}{optionRight}{symbol.ID.StrikePrice * 1000m:00000000}", $".{underlying}{expiryDate}{optionRight}{symbol.ID.StrikePrice.ToStringInvariant("G29")}");
+    }
+
+    /// <summary>
+    /// Asynchronously generates the brokerage and streaming symbols for a future.
+    /// </summary>
+    /// <param name="symbol">The Lean future symbol.</param>
+    /// <returns>A tuple with the brokerage symbol and the streaming market data symbol.</returns>
+    private async Task<(string brokerageSymbol, string brokerageStreamMarketDataSymbol)> GenerateFutureBrokerageSymbols(Symbol symbol)
+    {
+        var futureTicker = $"{symbol.ID.Symbol}{SymbolRepresentation.FuturesMonthLookup[symbol.ID.Date.Month]}{symbol.ID.Date.ToString("yy").Last()}";
+
+        var future = await _tastytradeApiClient.GetInstrumentFuture(futureTicker);
+
+        return (future.Symbol, future.StreamerSymbol);
+    }
+
+    /// <summary>
+    /// Converts a brokerage symbol back into a Lean symbol.
+    /// </summary>
+    /// <param name="brokerageSymbol">The brokerage symbol to convert.</param>
+    /// <param name="securityType">The security type (e.g., Equity, Option, Future).</param>
+    /// <param name="market">The market (e.g., "USA").</param>
+    /// <param name="expirationDate">The expiration date for derivatives, if applicable.</param>
+    /// <param name="strike">The strike price for options, if applicable.</param>
+    /// <param name="optionRight">The option right (Call or Put), if applicable.</param>
+    /// <returns>A new <see cref="Symbol"/> instance representing the Lean symbol.</returns>
+    /// <exception cref="NotImplementedException">Always thrown. Functionality not implemented yet.</exception>
+    public Symbol GetLeanSymbol(string brokerageSymbol, SecurityType securityType, string market, DateTime expirationDate = default, decimal strike = 0, OptionRight optionRight = OptionRight.Call)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Converts a Lean ticker symbol to a brokerage-compatible format by replacing periods with slashes.
+    /// </summary>
+    /// <param name="leanTicker">The Lean ticker symbol to be converted.</param>
+    /// <returns>The brokerage-compatible ticker symbol.</returns>
+    /// <remarks>
+    /// <para>This conversion is required because Lean uses dots (.) in symbols, 
+    /// while brokerages use slashes (/).</para>
+    /// <para><b>Example:</b></para>
+    /// <list type="bullet">
+    /// <item>
+    /// <term>Lean</term>
+    /// <description><c>Symbol.Create("BRK.B", SecurityType.Equity, Market.USA)</c></description>
+    /// </item>
+    /// <item>
+    /// <term>Brokerage</term>
+    /// <description><c>BRK/B</c></description>
+    /// </item>
+    /// </list>
+    /// </remarks>
+    private static string ToBrokerageTickerFormat(string leanTicker)
+    {
+        return leanTicker.Replace('.', '/');
+    }
+
+    /// <summary>
+    /// Converts an option symbol to brokerage format by removing any periods.
+    /// </summary>
+    /// <param name="leanTicker">The Lean ticker for the option.</param>
+    /// <returns>The brokerage-compatible ticker format for options.</returns>
+    private static string ToOptionBrokerageTickerFormat(string leanTicker)
+    {
+        return leanTicker.Replace(".", string.Empty);
+    }
+}
