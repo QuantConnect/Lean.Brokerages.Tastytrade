3f449e2e0faf642aed2b923644750f5cee9cda81
Romazes <romazes20@gmail.com>
2025-07-03 17:49:18 +0300

    test:feat: AssertOnMarket Open trading testCases test:feat: add attribute Explicit for trading tests

--- QuantConnect.TastytradeBrokerage.Tests/TastytradeBrokerageDataQueueHandlerTests.cs
+++ QuantConnect.TastytradeBrokerage.Tests/TastytradeBrokerageDataQueueHandlerTests.cs
@@ -42,19 +42,19 @@ public partial class TastytradeBrokerageTests
             yield return new TestCaseData(new[] { AAPL }, Resolution.Second);
             yield return new TestCaseData(new[] { AAPL }, Resolution.Minute);
 
-            var aaplOptionContract = Symbol.CreateOption(AAPL, AAPL.ID.Market, SecurityType.Option.DefaultOptionStyle(), OptionRight.Call, 200m, new DateTime(2025, 06, 20));
+            var aaplOptionContract = Symbol.CreateOption(AAPL, Market.USA, AAPL.SecurityType.DefaultOptionStyle(), OptionRight.Call, 200m, new DateTime(2025, 07, 03));
             yield return new TestCaseData(new[] { aaplOptionContract }, Resolution.Tick);
             yield return new TestCaseData(new[] { aaplOptionContract }, Resolution.Second);
             yield return new TestCaseData(new[] { aaplOptionContract }, Resolution.Minute);
 
             var spx = Symbols.SPX;
-            var spxOptionContract = Symbol.CreateOption(spx, spx.ID.Market, SecurityType.IndexOption.DefaultOptionStyle(), OptionRight.Call, 5635m, new DateTime(2025, 06, 20));
+            var spxOptionContract = Symbol.CreateOption(spx, spx.ID.Market, SecurityType.IndexOption.DefaultOptionStyle(), OptionRight.Call, 6195, new DateTime(2025, 07, 18));
             yield return new TestCaseData(new[] { spx }, Resolution.Tick);
             yield return new TestCaseData(new[] { spx, spxOptionContract }, Resolution.Tick);
             yield return new TestCaseData(new[] { spxOptionContract }, Resolution.Second);
             yield return new TestCaseData(new[] { spxOptionContract }, Resolution.Minute);
 
-            var spxw = Symbol.CreateOption(spx, "SPXW", spx.ID.Market, SecurityType.IndexOption.DefaultOptionStyle(), OptionRight.Call, 5935m, new DateTime(2025, 06, 30));
+            var spxw = Symbol.CreateOption(spx, "SPXW", spx.ID.Market, SecurityType.IndexOption.DefaultOptionStyle(), OptionRight.Call, 5935m, new DateTime(2025, 07, 18));
             yield return new TestCaseData(new[] { spxw }, Resolution.Tick);
 
             var SP500EMini = Symbol.CreateFuture(Futures.Indices.SP500EMini, Market.CME, new DateTime(2025, 09, 19));
@@ -109,7 +109,7 @@ public partial class TastytradeBrokerageTests
 
             lock (obj)
             {
-                if (incomingSymbolDataByTickType.Count == configs.Count && incomingSymbolDataByTickType.All(d => d.Value.Count > 2))
+                if (incomingSymbolDataByTickType.Count == configs.Count && incomingSymbolDataByTickType.Any(d => d.Value.Count > 2))
                 {
                     resetEvent.Set();
                 }
