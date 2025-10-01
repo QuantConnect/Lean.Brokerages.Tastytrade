475edaf3e5822ae28bafc8dd0539d8e6bf577374
Romazes <romazes20@gmail.com>
2025-10-01 17:28:44 +0300

    feat: additional remark to prop PriceEffect in Order DTO

--- QuantConnect.TastytradeBrokerage/Models/Orders/OrderResponse.cs
+++ QuantConnect.TastytradeBrokerage/Models/Orders/OrderResponse.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
  * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
  *
@@ -68,6 +68,9 @@ public class Order
     /// <summary>
     /// The price impact of the order (positive for credit, negative for debit).
     /// </summary>
+    /// <remarks>
+    /// Not set for <see cref="OrderType.Market"/> orders.
+    /// </remarks>
     public PriceEffect? PriceEffect { get; init; }
 
     /// <summary>
